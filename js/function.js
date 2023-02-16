e2b0ade1ff63b1b6da8da8666f53a01c111204d1
juancalca <juan.calcaprina@gmail.com>
2023-02-15 23:50:56 -0300

    Se agrega el Proyecto

---
+++ js/function.js
@@ -0,0 +1,25 @@
+
+window.onload=function () {    
+
+document.getElementById("boton").onclick = function() { 
+
+document.getElementById("demo").innerHTML = " Elio Inostroza, cel: 221036848";
+
+}
+document.getElementById("boton1").onclick = function() { 
+
+document.getElementById("demo1").innerHTML = " Romina Baez, cel: 221030008";
+    
+}
+document.getElementById("boton2").onclick = function() { 
+
+document.getElementById("demo2").innerHTML = " Lucia Rodriguez, cel: 236985848";
+        
+}
+document.getElementById("boton3").onclick = function() { 
+
+document.getElementById("demo3").innerHTML = " Juan Perez, cel: 278936848";
+            
+}
+}
+
